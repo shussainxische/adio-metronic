9cf78d798957f559958f1ce686c8735ee6f93c16
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:15:36 +0400

    Update index.ts

--- src/metronic/core/components/reparent/index.ts
+++ src/metronic/core/components/reparent/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTReparent } from './reparent';
+export type { KTReparentConfigInterface, KTReparentInterface } from './types';
