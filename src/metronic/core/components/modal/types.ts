537789309ef696f53ba256d50aa9ee5bcf7b3623
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:13:48 +0400

    Update types.ts

--- src/metronic/core/components/modal/types.ts
+++ src/metronic/core/components/modal/types.ts
@@ -1,19 +1,16 @@
-import { Placement } from "@popperjs/core";
-
-export declare type KTMenuItemToggleType = 'dropdown' | 'accordion';
-
-export declare type KTMenuItemTriggerType = 'hover' | 'click';
-
-export interface KTMenuConfigInterface {
-	dropdownZindex: string,
-	dropdownHoverTimeout: number,
-	dropdownPlacement: Placement;
-	dropdownOffset: string;
-  accordionExpandAll: boolean,
+export interface KTModalConfigInterface {
+	zindex?: string,
+	backdrop: boolean,
+  backdropClass: string,
+	backdropStatic: boolean,
+  keyboard: boolean,
+	disableScroll: boolean,
+	persistent: boolean;
+	focus: boolean;
+	hiddenClass: string;
 }
-
-export interface KTMenuInterface {		
-	disable(): void;
-	enable(): void;
-	update(): void;
+export interface KTModalInterface {	
+	show(): void;	
+	hide(): void;
+	toggle(): void;
 }
