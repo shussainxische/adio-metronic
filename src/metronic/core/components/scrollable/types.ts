b199417f79dac653ed40d495ff0a3a88b601dbe2
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:16:07 +0400

    Update types.ts

--- src/metronic/core/components/scrollable/types.ts
+++ src/metronic/core/components/scrollable/types.ts
@@ -1,19 +1,11 @@
-import { Placement } from "@popperjs/core";
-
-export declare type KTMenuItemToggleType = 'dropdown' | 'accordion';
-
-export declare type KTMenuItemTriggerType = 'hover' | 'click';
-
-export interface KTMenuConfigInterface {
-	dropdownZindex: string,
-	dropdownHoverTimeout: number,
-	dropdownPlacement: Placement;
-	dropdownOffset: string;
-  accordionExpandAll: boolean,
+export interface KTScrollableConfigInterface {
+	save: boolean,
+	dependencies: string,
+	wrappers: string,
+  offset: string
 }
 
-export interface KTMenuInterface {		
-	disable(): void;
-	enable(): void;
-	update(): void;
+export interface KTScrollableInterface {	
+	update(): void;	
+	getHeight(): string;
 }
