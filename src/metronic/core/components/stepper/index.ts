8025bc5f02abc31b26a9569c22d31ce872781ae7
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:29:16 +0400

    Update index.ts

--- src/metronic/core/components/stepper/index.ts
+++ src/metronic/core/components/stepper/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTStepper } from './stepper';
+export type { KTStepperConfigInterface, KTStepperInterface } from './types';
