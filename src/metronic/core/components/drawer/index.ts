cfab42bc6b39d341db39acd51b17dcabfda29f3f
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:08:17 +0400

    Update index.ts

--- src/metronic/core/components/drawer/index.ts
+++ src/metronic/core/components/drawer/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTDrawer } from './drawer';
+export type { KTDrawerConfigInterface, KTDrawerInterface } from './types';
