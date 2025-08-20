ac394568f875abb9d8212219724521bdf2b8d060
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:09:45 +0400

    Update index.ts

--- src/metronic/core/components/dropdown/index.ts
+++ src/metronic/core/components/dropdown/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTDropdown } from './dropdown';
+export type { KTDropdownConfigInterface, KTDropdownInterface } from './types';
