f4c1288989cd0e73b7eda53c0a6df6089c9e5b79
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:31:22 +0400

    Update index.ts

--- src/metronic/core/components/tabs/index.ts
+++ src/metronic/core/components/tabs/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTTabs } from './tabs';
+export type { KTTabsConfigInterface, KTTabsInterface } from './types';
