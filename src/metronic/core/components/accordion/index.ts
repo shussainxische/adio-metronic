0e43b6d9143a6962826da5a6a85432973f717a3a
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:01:53 +0400

    Update index.ts

--- src/metronic/core/components/accordion/index.ts
+++ src/metronic/core/components/accordion/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTAccordion } from './accordion';
+export type { KTAccordionConfigInterface, KTAccordionInterface } from './types';
