7ae33525b60240db7003e2509b174e6f5c59c2a0
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:06:32 +0400

    Update types.ts

--- src/metronic/core/components/dismiss/types.ts
+++ src/metronic/core/components/dismiss/types.ts
@@ -1,19 +1,12 @@
-import { Placement } from "@popperjs/core";
+export declare type KTDismissModeType = 'remove' | 'hide';
 
-export declare type KTMenuItemToggleType = 'dropdown' | 'accordion';
-
-export declare type KTMenuItemTriggerType = 'hover' | 'click';
-
-export interface KTMenuConfigInterface {
-	dropdownZindex: string,
-	dropdownHoverTimeout: number,
-	dropdownPlacement: Placement;
-	dropdownOffset: string;
-  accordionExpandAll: boolean,
+export interface KTDismissConfigInterface {
+	hiddenClass: string;
+	target: string;
+	interrupt: boolean;
+	mode?: KTDismissModeType;
 }
 
-export interface KTMenuInterface {		
-	disable(): void;
-	enable(): void;
-	update(): void;
+export interface KTDismissInterface {	
+	dismiss(): void;	
 }
