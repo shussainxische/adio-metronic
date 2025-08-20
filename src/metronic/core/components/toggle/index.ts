4a539c02a456173177f0aabb6e55a74eb7d02b5a
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:35:07 +0400

    Update index.ts

--- src/metronic/core/components/toggle/index.ts
+++ src/metronic/core/components/toggle/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTToggle } from './toggle';
+export type { KTToggleConfigInterface, KTToggleInterface } from './types';
