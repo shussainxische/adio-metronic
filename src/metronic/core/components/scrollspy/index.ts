e6a8e019cc6af3664d9ac9365576e043ab819b22
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-20 23:27:11 +0400

    Update index.ts

--- src/metronic/core/components/scrollspy/index.ts
+++ src/metronic/core/components/scrollspy/index.ts
@@ -1,2 +1,2 @@
-export { KTMenu } from './menu';
-export type { KTMenuConfigInterface, KTMenuInterface } from './types';
+export { KTScrollspy } from './scrollspy';
+export type { KTScrollspyConfigInterface, KTScrollspyInterface } from './types';
