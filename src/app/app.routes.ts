18387f284247b73753b2113d717a64ec77375b41
Sajjad Hussain <sajjad.hussain@xdigit.ai>
2025-08-21 10:55:05 +0400

    pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard',pathMatch: 'prefix' },
+     { path: 'dashboard', component: DashboardComponent },
+];
