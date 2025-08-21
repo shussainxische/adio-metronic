99f86429a170e7c8b88d6146ef4d6dbdf1e78388
Sajjad Hussain <sajjad.hussain@xdigit.ai>
2025-08-21 09:54:29 +0400

    loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,25 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    // MsalModule,
+    CommonModule,
+    TranslateModule,
+    LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'adio-metronic';
+  constructor(private router: Router, public loaderService: LoaderService) {
+  }
+
+    ngAfterViewInit(): void {
+    KTComponents.init();
+    KTLayout.init();
+  }
+
+  ngOnInit() {
+    //this.langService.initLanguage(); // Call the initialization function
+  }
 }
