1e68ef6171c6922d1da4fff2d6732c741213f141
Sajjad Hussain <sajjad.hussain@xdigit.ai>
2025-08-22 06:31:15 +0400

    translate srvc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
-// import { TranslateModule } from '@ngx-translate/core';
-import { LoaderComponent } from './components/ui/loader/loader.component';
-import { LoaderService } from './services/loader/loader.service';
+import { RouterOutlet, RoutesRecognized } from '@angular/router';
 import KTComponents from '../metronic/core/index';
 import KTLayout from '../metronic/app/layouts/demo1';
+// import { MsalModule, MsalService } from "@azure/msal-angular";
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+// import { LanguageService } from './services/language/language.service';
+import { Router, NavigationEnd, NavigationStart, NavigationError, NavigationCancel } from '@angular/router';
+import { LoaderComponent } from './components/ui/loader/loader.component';
+import { LoaderService } from './services/loader/loader.service';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, LoaderComponent, MainLayoutComponent],
+  imports: [RouterOutlet, CommonModule, TranslateModule, LoaderComponent, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'adio-metronic';
-  constructor(private router: Router, public loaderService: LoaderService) {
+  constructor(private router: Router, private translate: TranslateService, public loaderService: LoaderService) {
   
     this.router.events.subscribe(event => {
 
