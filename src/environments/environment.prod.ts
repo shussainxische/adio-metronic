4356d19c83bdfa02e4ec8e03ef0234bb46969139
shussainxische <sajjad.hussain@xdigit.ai>
2025-08-31 15:55:15 +0400

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   useMockData: false,
-  apiBaseUrl: 'http://localhost:5297/api',
+  apiBaseUrl: 'https://localhost:44354/api',
   inMemoryDataServiceUrl: 'api/'
 };
